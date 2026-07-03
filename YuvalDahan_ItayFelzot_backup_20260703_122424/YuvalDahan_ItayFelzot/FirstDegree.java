bc1b901d175906ad0e77ae501e22f59d6abead9f
Itay Felzot <itayfelzot1309@gmail.com>
2026-07-03 15:33:15 +0300

    Delete YuvalDahan_ItayFelzot_backup_20260703_122424/YuvalDahan_ItayFelzot directory

--- YuvalDahan_ItayFelzot_backup_20260703_122424/YuvalDahan_ItayFelzot/FirstDegree.java
+++
@@ -1,8 +0,0 @@
-package YuvalDahan_ItayFelzot;
-
-public class FirstDegree extends Lecturer {
-    public FirstDegree(String name, String id, Degree degree, String degreeName, float salary) {
-        super(name, id, degree, degreeName, salary);
-    }
-
-}
--- YuvalDahan_ItayFelzot_backup_20260703_122424/YuvalDahan_ItayFelzot/SecondDegree.java
+++
@@ -1,8 +0,0 @@
-package YuvalDahan_ItayFelzot;
-
-public class SecondDegree extends Lecturer {
-    public SecondDegree(String name, String id, Degree degree, String degreeName, float salary) {
-        super(name, id, degree, degreeName, salary);
-    }
-
-}
